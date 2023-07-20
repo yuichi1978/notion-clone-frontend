af05bb67e7bcddce65404a52a411553677e5b023
YuichiOta <rb19780519@gmail.com>
2023-07-20 18:26:01 +0900

    baseurl

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 
 // const BASE_URL = "http://localhost:5000/api/v1";
-const BASE_URL = process.env.NOTION_PUBLIC_API_BASEURL;
+// const BASE_URL = process.env.NOTION_PUBLIC_API_BASEURL;
 const getToken = () => localStorage.getItem("token");
 
 const axiosClient = axios.create({
-  baseURL: BASE_URL,
+  baseURL: process.env.NOTION_PUBLIC_API_BASEURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 // APIを叩く前に前処理を行う
